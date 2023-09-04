6009513a9224910e352b7497008e8559a1e8d821
ThienCNTT <45612350+thiencntt@users.noreply.github.com>
2023-09-04 13:47:39 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten cua ban: ");
+            string ten = Console.ReadLine();
+            Console.WriteLine("Hello {0}!",ten);
+            Console.ReadKey();
+        }
+    }
+}
